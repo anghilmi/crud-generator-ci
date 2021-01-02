1f5af447ed789a1e3f60ad66b8320a44a8a0a646
Muhammad Anis Al Hilmi <alhilmi1@gmail.com>
2021-01-02 14:48:22 +0700

    Update create_config_pagination.php
    li pagination menyesuaikan BS4

--- core/create_config_pagination.php
+++ core/create_config_pagination.php
@@ -3,8 +3,9 @@
 $string = "<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /* 
- * Pagination Config Bootstrap 3 CSS Style
+ * Pagination Config Bootstrap 4 CSS Style
  * harviacode.com
+ * 
  */
 
 \$config['query_string_segment'] = 'start';
@@ -13,25 +14,25 @@ $string = "<?php  if ( ! defined('BASEPATH')) exit('No direct script access allo
 \$config['full_tag_close'] = '</ul></nav>';
 
 \$config['first_link'] = 'First';
-\$config['first_tag_open'] = '<li>';
+\$config['first_tag_open'] = '<li class=\"page-item \">';
 \$config['first_tag_close'] = '</li>';
 
 \$config['last_link'] = 'Last';
-\$config['last_tag_open'] = '<li>';
+\$config['last_tag_open'] = '<li class=\"page-item \">';
 \$config['last_tag_close'] = '</li>';
 
 \$config['next_link'] = 'Next';
-\$config['next_tag_open'] = '<li>';
+\$config['next_tag_open'] = '<li class=\"page-item \">';
 \$config['next_tag_close'] = '</li>';
 
 \$config['prev_link'] = 'Prev';
-\$config['prev_tag_open'] = '<li>';
+\$config['prev_tag_open'] = '<li class=\"page-item \">';
 \$config['prev_tag_close'] = '</li>';
 
-\$config['cur_tag_open'] = '<li class=\"active\"><a>';
+\$config['cur_tag_open'] = '<li class=\"page-item active\"><a class=\"page-link\">';
 \$config['cur_tag_close'] = '</a></li>';
 
-\$config['num_tag_open'] = '<li>';
+\$config['num_tag_open'] = '<li class=\"page-item \">';
 \$config['num_tag_close'] = '</li>';
 
 
