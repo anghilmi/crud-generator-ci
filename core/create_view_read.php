88ad5179bee23eb3a0123112682599c0ffeb50fa
Muhammad Anis Al Hilmi <alhilmi1@gmail.com>
2021-01-02 14:47:22 +0700

    Update create_view_read.php
    remove integrity

--- core/create_view_read.php
+++ core/create_view_read.php
@@ -8,7 +8,7 @@ $string = "<!doctype html>
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
 
     <!-- Bootstrap CSS -->
-    <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.0/css/bootstrap.min.css\" integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">
+    <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.0/css/bootstrap.min.css\" >
 
     <title></title>
   </head>
